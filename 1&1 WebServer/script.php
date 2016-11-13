67560d55fcbce5e43ef7760f85b372df58d67199
Harsh Baid <harsh@harshbaid.com>
2016-11-13 09:09:35 -0500

    Fixed Coca Cola Bug

--- 1&1 WebServer/script.php
+++ 1&1 WebServer/script.php
@@ -1,8 +1,6 @@
 <?php
 
-
-
-$xml = file_get_contents("http://ws.nasdaqdod.com/v1/NASDAQAnalytics.asmx/GetEndOfDayData?Symbols=" . $_GET['ticker'] . "&StartDate=11/10/16&EndDate=11/11/16&_token=BC2B181CF93B441D8C6342120EB0C971&MarketCenters=Q");
+$xml = file_get_contents("http://ws.nasdaqdod.com/v1/NASDAQAnalytics.asmx/GetEndOfDayData?Symbols=" . $_GET['ticker'] . "&StartDate=11/10/16&EndDate=11/11/16&_token=BC2B181CF93B441D8C6342120EB0C971&MarketCenters=Q,N");
 
 function nice_number($n) {
 	// first strip any formatting;
@@ -18,19 +16,34 @@ function nice_number($n) {
 
 	return number_format($n);
 }
+
+function encrypt($text){
+	$key = "b6 a0 26 45 97 43 ff 74 27 e9 6e 85 3e cd 18 7c 4c 98 56 12 ff 70 ab 75 25 bd 32 54 90 1a b1 7e";
+	$IV = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC), MCRYPT_RAND); 
+	return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $text, MCRYPT_MODE_CBC, $IV)."-[--IV-[-".$IV); 
+}
+
+
 $parsed = new SimpleXMLElement($xml);
 $prev = floatval($parsed->EndOfDayPriceCollection[0]->Prices[0]->EndOfDayPrice[0]->Close);
-
 $val = floatval($prev - $parsed->EndOfDayPriceCollection[0]->Prices[0]->EndOfDayPrice[1]->Close);
+$str = "";
+
 if($val > 0){
-	echo "G ";
-	echo "+" . round($val,2) . "(+".round(($val/$prev)*100, 2)."%) ";
+	$str .= "G ";
+	$str .= "+" . round($val,2) . "(+".round(($val/$prev)*100, 2)."%) ";
 } else {
-	echo "R "; 
-	echo round($val,2) . "(".round(($val/$prev)*100, 2)."%) ";
+	$str .= "R "; 
+	$str .= round($val,2) . "(".round(($val/$prev)*100, 2)."%) ";
 }
-echo $parsed->EndOfDayPriceCollection[0]->Prices[0]->EndOfDayPrice[1]->Open . " ";
-echo $parsed->EndOfDayPriceCollection[0]->Prices[0]->EndOfDayPrice[1]->Close . " ";
-echo nice_number($parsed->EndOfDayPriceCollection[0]->Prices[0]->EndOfDayPrice[0]->Volume) . " ";
+$str .= $parsed->EndOfDayPriceCollection[0]->Prices[0]->EndOfDayPrice[1]->Open . " ";
+$str .= $parsed->EndOfDayPriceCollection[0]->Prices[0]->EndOfDayPrice[1]->Close . " ";
+$str .= nice_number($parsed->EndOfDayPriceCollection[0]->Prices[0]->EndOfDayPrice[0]->Volume) . " ";
+
+if(isset($_GET['encrypt'])){
+	echo encrypt($str);
+} else {
+	echo $str;
+}
 
 ?>
